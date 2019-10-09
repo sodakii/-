76bb47cd96ac8547060be6c864f45d92841ac12b
黄昕 <1151963969@qq.com>
2019-10-09 10:54:36 +0800

    【更新】2019-10-09 添加表单方法

--- library.js
+++ library.js
@@ -79,3 +79,65 @@ axios.all([getUserAccount(), getUserPermissions()])
 
 
 //4.ES6 新特性  https://juejin.im/post/5ca2e1935188254416288eb2#heading-16
+
+//5.HTML5 canvas  https://segmentfault.com/a/1190000016031115
+
+//6.二级联动下拉框
+
+//html 部分
+<form name="lineandstation">
+    线路选择:
+        <select name="lines" id="lines" onchange="getStationName()">
+        <option value="0">请选择线路</option>
+    </select> 站点选择：
+        <select name="station">
+        <option value="0">请选择车站</option>
+    </select>
+</form>
+
+//js部分
+var lineNum = ["101", "102"];
+var collect = document.getElementById("lines")
+var old = collect.innerHTML
+function aa() {
+    var lineNu = " "
+    for (var j = 0; j < lineNum.length; j++) {
+        lineNu += '<option>' + lineNum[j] + '</option>';
+    }
+    collect.innerHTML = old + lineNu;
+} //网页加载时执行此函数，为线路选择框动态添加选项      
+aa();
+var stationName = [
+    ["百万庄西口", "百万庄中街", "百万庄东口", "展览路", "阜成门外", "阜成门", "阜成门内", "白塔寺", "西四路口东", "西安门", "北海", "故宫", "沙滩路口西", "美术馆东", "东四路口东", "朝内小街", "朝阳门内", "朝阳门外", "神路街", "东大桥路口东", "关东店", "呼家楼西", "小庄路口东", "红庙路口西", "红庙路口东"],
+    ["动物园枢纽站", "二里沟", "百万庄", "甘家口大厦", "甘家口东", "阜外西口", "展览路", "阜成门外", "阜成门", "阜成门内", "白塔寺", "西四路口西", "缸瓦市", "甘石桥", "西单商场", "西单路口南", "宣武门内", "校场口", "菜市口北", "果子巷", "虎坊桥路口南", "太平街北口", "太平街", "陶然桥北", "永定门长途汽车站", "北京南站"]
+];
+
+function getStationName() {
+    var line_num = document.lineandstation.lines;
+    var station_name = document.lineandstation.station;
+    var lineStation = stationName[line_num.selectedIndex - 1];
+    station_name.length = 1;
+    for (var i = 0; i < lineStation.length; i++) {
+        station_name[i + 1] = new Option(lineStation[i], lineStation[i]);
+    }
+} //为站点选择框根据线路选择框的值动态添加选项
+
+//7. 表单序列化方法
+$.fn.serializeObject = function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+// 8.表单清空 
+// 清空表单 （“addGoodsForm”是表单的id）
+$("#addGoodsForm")[0].reset();
